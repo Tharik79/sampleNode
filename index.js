4da79de90467fabb680ee7db29975afd316a8b01
tharikuthuman <thaarik@gmail.com>
2021-12-01 10:05:05 +0530

    organised into helper and seperate appMovies into moviejs

--- index.js
+++ index.js
@@ -37,116 +37,12 @@ async function createConnection(){
 }
 export const client = await createConnection();
 
-app.get("/movies", async (request, response) => {
-// request -> query praams
-  console.log(request.query);
-// to do filter by language , to write additional code before response.send.
 
-// const {language} = request.query;
-// const {rating} = request.query;
-// console.log(language);
-
-//to include if condition to get all movies and filter list of movies.
-  // if(language){
-  //   const filterMovies = movies.filter((mv) => mv.language == language);
-  //   response.send(filterMovies); //type on browser, localhost9000/movies?language=tamil
-  // } else {
-  //     response.send(movies);
-    //   }
-
-    // To rewrite the above code in easy shortern format without else part, below:
-
-    // let filterMovies = movies; // it is fullfilling all filter conditions .
-    // if(language) {
-    //   filterMovies = filterMovies.filter((mv) => mv.language === language );
-
-    // }
-    // if(rating) {
-    //   filterMovies = filterMovies.filter((mv) => mv.rating === +rating );
-
-    // }
-
-    const filter = request.query;
-    console.log(filter);
-    if (filter.rating) {
-
-      filter.rating = parseFloat(filter.rating);
-    }
-    //to write code for filter movies by language and ratings in mongodb
-    //db.movies.find({language:tamil, rating:8}) will be written using below code using nodejs.
-    const filterMovies = await getMovies(filter);//cursor -> array
-    console.log(filterMovies);
-    // cursor -> pagination 1,2,3,4,5..
-
-    response.send(filterMovies);
-});
-
-app.post("/movies", async (request, response) => {
-  const data = request.body;
-  //create movies -> db.movies.insertMany(data)
-  const result = await createMovies(data);
-  response.send(result);
-});
-
-app.get("/movies/:id", async (request, response) => {
-  console.log(request.params);
-  const {id} = request.params;
-
-  //db.movie = findOne
-  const movie = await getMovieById(id);
-  
-  // const movie = movies.find((mv) => mv.id === id);
-  console.log(movie);
-  //response.send(movie);
-  // how to print message, no matching movies found if wrong movie id entered.
-    movie
-    ? response.send(movie) : response.status(404).send({message: "No Matching Movies Found"});
-
-
-});
+app.use("/movies", moviesRouter)
 
 app.listen(PORT, () => console.log("App is started in", PORT)
-);
-
-//Task given, soving below:
-app.delete("/movies/:id", async (request, response) => {
-  console.log(request.params);
-  const {id} = request.params;
-
-  //db.movie = findOne
-  const movie = await deleteMovieById(id);
-  
-  // const movie = movies.find((mv) => mv.id === id);
-  console.log(movie);
-  //response.send(movie);
-  // how to print message, no matching movies found if wrong movie id entered.
-    movie
-    ? response.send(movie) : response.status(404).send({message: "No Matching Movies Found"});
-
-    result.deleteCount > 0 
-    ? response.send(result)
-    :response.status(404).send({message: "No matching movies Found"});
- } );
+    );
 
 
- app.put("/movies/:id", async (request, response) => {
-  console.log(request.params);
-  const {id} = request.params;
-  const data = request.body;
-  //db.movies.updateOne({rating:9}, {$set:data})
-  const movie = await updateMovieById(id, data);
-  
-  // const movie = movies.find((mv) => mv.id === id);
-  console.log(movie);
-  //response.send(movie);
-  // how to print message, no matching movies found if wrong movie id entered.
-    // movie
-    // ? response.send(movie) : response.status(404).send({message: "No Matching Movies Found"});
-
-    // result.deleteCount > 0 
-    // ? response.send(result)
-    // :response.status(404).send({message: "No matching movies Found"});
-    response.send(movie);
- } );
 
 
